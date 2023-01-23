0eee7a6f1779c4982d806b934468532935eb23a3
Franlex Ramirez <franlex.ramirez@gmail.com>
2023-01-22 22:26:33 -0400

    Todo list

--- todos/todos.js
+++ todos/todos.js
@@ -1,21 +1,80 @@
 const formTodo = document.querySelector('#form-todos');
 const formInput = document.querySelector('#form-input');
 const todosList = document.querySelector('#todos-list');
-
 const user = JSON.parse(localStorage.getItem('user'));
-console.log(user);
+const closeBtn = document.querySelector('#cerrar-btn');
+
+if (!user) {
+    window.location.href = '../home/index.html';
+}
 
 formTodo.addEventListener('submit', async e => {
     e.preventDefault();
-    await fetch('http://localhost:3000/todos', {
+    const responseJSON = await fetch('http://localhost:3000/todos', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify({text: formInput.value, user: user.username}),
+        body: JSON.stringify({text: formInput.value, user: user.username, checked: false}),
     });
+
+    const response = await responseJSON.json();
+    console.log(response);
 
     //Crear la lista de tareas en el HTML
     const listItem = document.createElement('li');
-    
+    listItem.innerHTML = `
+    <li class="todo-item" id="${response.id}">
+        <button class="delete-btn">&#10006;</button>
+            <p>${response.text}</p>
+        <button class="check-btn">&#10003;</button>
+    </li>
+    `;
+    todosList.append(listItem);
+    formInput.value = '';
 });
+
+todosList.addEventListener('click', async e => {
+    if (e.target.classList.contains('delete-btn')) {
+        const id = e.target.parentElement.id;
+        await fetch(`http://localhost:3000/todos/${id}`, {method: 'DELETE'});
+        e.target.parentElement.remove();
+    } else if (e.target.classList.contains('check-btn')) {
+        const id = e.target.parentElement.id;
+        await fetch(`http://localhost:3000/todos/${id}`, {
+        method: 'PATCH',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({checked: e.target.parentElement.children[1].classList.contains('check-todo') ? false : true}),
+    });
+    e.target.parentElement.children[1].classList.toggle('check-todo');
+    }
+});
+
+
+//Cerrar sesion
+closeBtn.addEventListener('click', async e => {
+    localStorage.removeItem('user');
+    window.location.href = '../home/index.html';
+});
+
+
+const getTodos = async () => {
+    const response = await fetch('http://localhost:3000/todos', {method: 'GET'});
+    const todos = await response.json();
+    const userTodos = todos.filter(todo => todo.user === user.username);
+    userTodos.forEach(todo => {
+    const listItem = document.createElement('li');
+    listItem.innerHTML = `
+    <li class="todo-item" id="${todo.id}">
+        <button class="delete-btn">&#10006;</button>
+            <p ${todo.checked ? 'class="check-todo"' : null}>${todo.text}</p>
+        <button class="check-btn">&#10003;</button>
+    </li>
+    `;
+    todosList.append(listItem);
+    });
+}
+
+getTodos();
